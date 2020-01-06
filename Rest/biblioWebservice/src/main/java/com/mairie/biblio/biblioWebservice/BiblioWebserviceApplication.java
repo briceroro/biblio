fa367589f43c7318110072e342315bf59f99b21e
briceroro <bromming@mailfence.com>
2020-01-06 16:55:35 +0100

    first commit

---
+++ Rest/biblioWebservice/src/main/java/com/mairie/biblio/biblioWebservice/BiblioWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.mairie.biblio.biblioWebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblioWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblioWebserviceApplication.class, args);
+	}
+
+}
---
+++ Rest/biblioWebservice/src/test/java/com/mairie/biblio/biblioWebservice/BiblioWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mairie.biblio.biblioWebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
